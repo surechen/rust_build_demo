ec2e03a0b70586da388571720cb1f61bdc6ff130
surechen <surechen17@gmail.com>
2021-03-18 10:53:41 +0800

    add test for cargo-geiger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     println!("Hello, world!");
     let mut n: i32 = 64;
-    let p1 = &num as *const i32;
-    let r2 = &mut num as *mut i32;
+    let p1 = &n as *const i32;
+    let p2 = &mut n as *mut i32;
 
     unsafe {
-        println!("r1 is: {}", *r1); 
-        println!("r2 is: {}", *r2);
+        println!("r1 is: {}", *p1);
+        println!("r2 is: {}", *p2);
     }
 }
