b63914de5236b05c6bd4ade3bbdc8debe1c04dc9
surechen <surechen17@gmail.com>
2021-03-18 10:47:34 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    let mut n: i32 = 64;
+    let p1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1); 
+        println!("r2 is: {}", *r2);
+    }
 }
