1ed833dd866e74581a5f6ae327b58a1b85ed115e
surechen <chenshuo17@huawei.com>
2021-05-10 09:33:08 +0800

    edit

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,29 @@
 #[macro_use]
 extern crate quickcheck;
 
-fn reverse<T: Clone>(xs: &[T]) -> Vec<T> {
-    let mut rev = vec![];
-    for x in xs.iter() {
-        rev.insert(0, x.clone())
-    }
-    rev
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::reverse;
+    fn reverse<T: Clone>(xs: &[T]) -> Vec<T> {
+        let mut rev = vec![];
+        for x in xs.iter() {
+            rev.insert(0, x.clone())
+        }
+        rev
+    }
+
     quickcheck! {
         fn prop(xs: Vec<u32>) -> bool {
             xs == reverse(&reverse(&xs))
+        }
+    }
+
+    //proptest
+    use crate::parse_date;
+    use proptest::prelude::*;
+    proptest! {
+        #[test]
+        fn doesnt_crash(s in "\\PC*") {
+            parse_date(&s);
         }
     }
 }
@@ -40,19 +49,10 @@ fn parse_date(s: &str) -> Option<(u32, u32, u32)> {
     })
 }
 
-//proptest
-use proptest::prelude::*;
-
-proptest! {
-    #[test]
-    fn doesnt_crash(s in "\\PC*") {
-        parse_date(&s);
-    }
-}
-
 fn sanitizer() {
     let x = vec![1, 2, 3, 4];
     let _y = unsafe { *x.as_ptr().offset(6) };
+    println!("{}", _y)
 }
 
 fn main() {
@@ -71,6 +71,5 @@ fn main() {
     assert_eq!(result, Ok((b'h', dangerous::input(b"ello"))));
 
     sanitizer();
-    reverse(&vec![1, 2, 3]);
     parse_date("2021-02-02");
 }
